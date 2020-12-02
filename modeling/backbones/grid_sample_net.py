ca6d5a7c9156341c037355e188bf431a54638e98
Patrick Barton <patrick_barton@me.com>
2020-12-02 17:28:54 +0100

    use scaled dem gradient as grid

--- modeling/backbones/grid_sample_net.py
+++ modeling/backbones/grid_sample_net.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torch.nn.functional import grid_sample
 from modeling.reusable_blocks import Bottleneck, BasicBlock, conv1x1, conv3x3
+from kornia.filters.sobel import SpatialGradient
 
 
 class GridSampleNet(nn.Module):
@@ -26,19 +27,18 @@ class GridSampleNet(nn.Module):
         self.bn2 = norm_layer(self.inplanes)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-        self.layer2 = self._make_layer(Bottleneck, self.inplanes, 64, 3, stride=2)
+        self.layer2 = self._make_layer(Bottleneck, self.inplanes, 64, 4, stride=2)
 
-        self.dem_grads = nn.Conv2d(1, 2, kernel_size=3, stride=2, padding=1)
-        self.avgpool = nn.AvgPool2d(2)
-        self.feature_squeeze = conv1x1(64*Bottleneck.expansion, 2)
-        self.grid_layer = self._make_layer(Bottleneck, 4, 8, 3)
-        self.grid_squeeze = conv1x1(32, 2)
+        self.spatial_grad = SpatialGradient()
+        self.grad_scale = nn.Parameter(torch.rand(1))
+        self.avgpool = nn.AvgPool2d(4)
+        self.feature_squeeze = conv1x1(64*Bottleneck.expansion, 64)
 
         merge_outplanes = 64 * Bottleneck.expansion
-        self.bn3 = norm_layer(3 * merge_outplanes)
+        self.bn3 = norm_layer(merge_outplanes + 2*64)
         self.tanh = nn.Tanh()
-        self.merge = conv1x1(3 * merge_outplanes, merge_outplanes) #BasicBlock(merge_inplanes, merge_outplanes)
-        self.postprocess = self._make_layer(Bottleneck, merge_outplanes, 64, 2)
+        self.merge = conv1x1(merge_outplanes + 2*64, merge_outplanes)
+        self.postprocess = self._make_layer(Bottleneck, merge_outplanes, 64, 4)
         self.outplanes = merge_outplanes
 
         for m in self.modules():
@@ -70,35 +70,35 @@ class GridSampleNet(nn.Module):
         ]
 
     def forward(self, x):
-        x = x[:, :-1, :, :]
+        features = x[:, :-1, :, :]
         dem = x[:, [-1], :, :]
 
         stages = self.get_stages()
 
         for stage in stages:
-            x = stage(x)
+            features = stage(features)
 
         # Calculate grids
-        dem_grads = self.dem_grads(dem)
-        dem_grads = self.avgpool(dem_grads)
+        dem = self.spatial_grad(dem).squeeze(dim=1)
+        dem = self.avgpool(dem)
+        dem *= self.grad_scale
 
-        features = self.feature_squeeze(x)
-        grid = self.grid_layer(torch.cat([dem_grads, features], dim=1))  # Todo: check because of batch norm
-        grid = self.grid_squeeze(grid)
-        grid *= grid.shape[2] / 64  # rescale to work with any patch size since grid is relativ to patch size -
-        grid1 = grid.permute(0, 2, 3, 1)
+        dem *= dem.shape[2] / 64  # rescale to work with any patch size since grid is relativ to patch size -
+        grid1 = dem.permute(0, 2, 3, 1)
         grid2 = -grid1
 
-        out_dir1 = x
-        out_dir2 = x
-        sum_dir1 = torch.zeros_like(x)
-        sum_dir2 = torch.zeros_like(x)
+        # propagate features along dem gradient field
+        out = self.feature_squeeze(features)
+        out_dir1 = out
+        out_dir2 = out
+        sum_dir1 = torch.zeros_like(out)
+        sum_dir2 = torch.zeros_like(out)
         for i in range(self.iterations):
             out_dir1 = grid_sample(out_dir1, grid1)
             out_dir2 = grid_sample(out_dir2, grid2)
             sum_dir1 += out_dir1
             sum_dir2 += out_dir2
-        out = torch.cat([x, sum_dir1, sum_dir2], dim=1)
+        out = torch.cat([features, sum_dir1, sum_dir2], dim=1)
         out = self.bn3(out)
         out = self.tanh(out)
         out = self.merge(out)
